f6db0b3b321195a922650e814e7588c38115ee7f
rahul <rahulpopuri@gmail.com>
2018-04-06 15:49:14 -0400

    Changed website layout

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,5 @@
 <template>
   <div id="app">
-    <nav class="navbar ">
-      <div class="navbar-brand">
-        <div class="navbar-burger burger" onclick="document.querySelector('.navbar-menu').classList.toggle('is-active'); document.querySelector('.navbar-burger').classList.toggle('is-active');">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-      <div class="navbar-menu">
-        <div class="navbar-start">
-          <div class="navbar-item is-hoverable">
-             <router-link to="/" class="navbar-item is-active">Home</router-link>
-          </div>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link" >
-              Projects
-            </a>
-            <div class="navbar-dropdown ">
-              <a class="navbar-item " href="https://play.google.com/store/apps/details?id=ca.bubblewrapstudios.mailinglist" target="_blank">
-                Mailing List
-              </a>
-              <a class="navbar-item " href="http://twine.bubblewrapstudios.ca" target="_blank">
-                Twine
-              </a>
-              <a class="navbar-item " href="https://github.com/rahulpopuri/tvmaze-java" target="_blank">
-                TvMaze API
-              </a>
-              <a class="navbar-item " href="https://github.com/rahulpopuri/plants" target="_blank">
-                Plants
-              </a>
-              <a class="navbar-item " href="http://twine.bubblewrapstudios.ca" target="_blank">
-                URL Shortener
-              </a>
-            </div>
-          </div>
-          <div class="navbar-item">
-             <a href="https://500px.com/rahulpopuri" target="_blank">Photography</a>
-          </div>
-        </div>
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="field is-grouped">
-              <p class="control">
-                <a href="https://drive.google.com/open?id=1RXFHtcyVrSRpmWZc4NIdqGLvkunBQZt0" target="_blank">
-                    Resume
-                </a>
-              </p>
-              <p class="control">
-                <a class="icon" href="https://github.com/rahulpopuri">
-                    <i class="fa fa-github" aria-hidden="true"></i>
-                </a>
-              </p>
-              <p class="control">
-                <a class="icon" href="https://www.linkedin.com/in/rahulpopuri/">
-                    <i class="fa fa-linkedin" aria-hidden="true"></i>
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
     <router-view/>
   </div>
 </template>
@@ -70,9 +8,43 @@
 export default {
   name: 'App'
 }
+
+function closeDropdownsIfAnyClick (ev) {
+  'use strict'
+
+  var tgt = ev.target || ev.srcElement
+
+  // close all (but allow open just clicked)
+  document.querySelectorAll('.has-dropdown input[type="checkbox"]').forEach(
+    function (chb) {
+      if (chb.id !== tgt.id && chb.id !== tgt.getAttribute('for')) { chb.checked = false }
+    }
+  )
+
+  return true
+}
+
+document.querySelector('body').addEventListener('click', closeDropdownsIfAnyClick)
 </script>
 
 <style lang='sass' scoped>
 @import 'mq'
+.navbar-item a
+    font-weight:bold
 
+a.navbar-item,
+.navbar-link
+    color: black
+    padding: 0.5rem 1.75rem
+    font-weight: normal
+
+a.navbar-item,
+a.navbar-link
+    &:hover,
+    &.is-active
+        background-color: $navbar-item-hover-background-color
+        color: $navbar-item-hover-color
+
+.icon
+  color: black
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,32 +1,89 @@
 <template>
-    <div class="home has-navbar-fixed-top">
+    <div class="home">
         <section class="hero is-light is-fullheight">
-            <div class="hero-head">
-                <div class="container is-fluid has-text-centered">
-                    <img src="../assets/rahul.jpg" width="200" height="400" alt="Rahul Popuri">
-                    <h1 class="title">
-                        Rahul Popuri
-                    </h1>
-                    <h2 class="subtitle">
-                        Java Developer
-                    </h2>
-                </div>
-            </div>
             <div class="hero-body">
                 <div class="container is-fluid has-text-centered">
-                    <h2 class="title">
-                        Skills
-                    </h2>
-                    <span class="tag is-info">Java</span>
-                    <span class="tag is-info">Spring Boot</span>
-                    <span class="tag is-info">SQL Server</span>
-                    <span class="tag is-info">MySQL</span>
-                    <span class="tag is-info">Google Cloud</span>
-                    <span class="tag is-warning">Android</span>
-                    <span class="tag is-warning">Tableau</span>
-                    <span class="tag is-warning">Python</span>
-                    <span class="tag is-warning">MongoDB</span>
-                    <span class="tag is-warning">Redis</span>
+                    <section class="section">
+                        <div class="columns is-desktop is-vcentered">
+                            <div class="column has-text-right-desktop">
+                                <img src="../assets/rahul.jpg" width="200" height="200" alt="Rahul Popuri">
+                            </div>
+                            <div class="column has-text-left-desktop">
+                                <section class="section">
+                                    <h1 class="title">
+                                        Rahul Popuri
+                                    </h1>
+                                    <h1 class="subtitle is-size-6">
+                                        Backend Developer
+                                    </h1>
+                                    <a class="has-text-black has-text-weight-semi-bold" href="https://drive.google.com/open?id=1RXFHtcyVrSRpmWZc4NIdqGLvkunBQZt0" target="_blank">
+                                        Resume
+                                    </a>
+                                    <a class="icon" href="https://github.com/rahulpopuri">
+                                        <i class="fa fa-github" aria-hidden="true"></i>
+                                    </a>
+                                    <a class="icon" href="https://www.linkedin.com/in/rahulpopuri/">
+                                        <i class="fa fa-linkedin" aria-hidden="true"></i>
+                                    </a>
+                                    <a class="icon" href="https://500px.com/rahulpopuri">
+                                        <i class="fa fa-500px" aria-hidden="true"></i>
+                                    </a>
+                                </section>
+                                <section class="section">
+                                    <div class="dropdown">
+                                        <div class="dropdown-trigger">
+                                            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu2">
+                                            <span>Projects</span>
+                                            <span class="icon is-small">
+                                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                                            </span>
+                                            </button>
+                                        </div>
+                                        <div class="dropdown-menu" id="dropdown-menu2" role="menu">
+                                            <div class="dropdown-content has-text-left">
+                                                <div class="dropdown-item">
+                                                    <a href="https://play.google.com/store/apps/details?id=ca.bubblewrapstudios.mailinglist" target="_blank">
+                                                        Mailing List
+                                                    </a>
+                                                </div>
+                                                <hr class="dropdown-divider">
+                                                <a class="dropdown-item " href="http://twine.bubblewrapstudios.ca" target="_blank">
+                                                    Twine
+                                                </a>
+                                                <hr class="dropdown-divider">
+                                                <a class="dropdown-item " href="https://github.com/rahulpopuri/tvmaze-java" target="_blank">
+                                                    TvMaze API
+                                                </a>
+                                                <hr class="dropdown-divider">
+                                                <a class="dropdown-item " href="https://github.com/rahulpopuri/plants" target="_blank">
+                                                    Plants
+                                                </a>
+                                                <hr class="dropdown-divider">
+                                                <a class="dropdown-item " href="http://twine.bubblewrapstudios.ca" target="_blank">
+                                                    URL Shortener
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+                            </div>
+                        </div>
+                    </section>
+                    <section class="section">
+                        <h2 class="title">
+                            Skills
+                        </h2>
+                        <span class="tag is-info ">Java</span>
+                        <span class="tag is-info">Spring Boot</span>
+                        <span class="tag is-info">SQL Server</span>
+                        <span class="tag is-info">MySQL</span>
+                        <span class="tag is-info">Google Cloud</span>
+                        <span class="tag is-warning">Android</span>
+                        <span class="tag is-warning">Tableau</span>
+                        <span class="tag is-warning">Python</span>
+                        <span class="tag is-warning">MongoDB</span>
+                        <span class="tag is-warning">Redis</span>
+                    </section>
                 </div>
             </div>
         </section>
@@ -36,12 +93,44 @@
 <script>
 
 export default {
-  name: 'about',
-  data: function () {
-    return {
-    }
-  }
+  name: 'about'
 }
+document.addEventListener('DOMContentLoaded', function () {
+  var $dropdowns = getAll('.dropdown:not(.is-hoverable)')
+
+  if ($dropdowns.length > 0) {
+    $dropdowns.forEach(function ($el) {
+      $el.addEventListener('click', function (event) {
+        event.stopPropagation()
+        $el.classList.toggle('is-active')
+      })
+    })
+
+    document.addEventListener('click', function (event) {
+      closeDropdowns()
+    })
+  }
+
+  function closeDropdowns () {
+    $dropdowns.forEach(function ($el) {
+      $el.classList.remove('is-active')
+    })
+  }
+
+  // Close dropdowns if ESC pressed
+  document.addEventListener('keydown', function (event) {
+    var e = event || window.event
+    if (e.keyCode === 27) {
+      closeDropdowns()
+    }
+  })
+
+  // Functions
+
+  function getAll (selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0)
+  }
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
