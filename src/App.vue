9e6ba42df1cb52c2b317b1ba48d76bbfdd246507
rahul <rahulpopuri@gmail.com>
2018-04-06 02:18:26 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <nav class="navbar ">
+      <div class="navbar-brand">
+        <div class="navbar-burger burger" onclick="document.querySelector('.navbar-menu').classList.toggle('is-active'); document.querySelector('.navbar-burger').classList.toggle('is-active');">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <div class="navbar-item is-hoverable">
+             <router-link to="/" class="navbar-item is-active">Home</router-link>
+          </div>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link" >
+              Projects
+            </a>
+            <div class="navbar-dropdown ">
+              <a class="navbar-item " href="https://play.google.com/store/apps/details?id=ca.bubblewrapstudios.mailinglist" target="_blank">
+                Mailing List
+              </a>
+              <a class="navbar-item " href="http://twine.bubblewrapstudios.ca" target="_blank">
+                Twine
+              </a>
+              <a class="navbar-item " href="https://github.com/rahulpopuri/tvmaze-java" target="_blank">
+                TvMaze API
+              </a>
+              <a class="navbar-item " href="https://github.com/rahulpopuri/plants" target="_blank">
+                Plants
+              </a>
+              <a class="navbar-item " href="http://twine.bubblewrapstudios.ca" target="_blank">
+                URL Shortener
+              </a>
+            </div>
+          </div>
+          <div class="navbar-item">
+             <a href="https://500px.com/rahulpopuri" target="_blank">Photography</a>
+          </div>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="field is-grouped">
+              <p class="control">
+                <a href="https://drive.google.com/open?id=1RXFHtcyVrSRpmWZc4NIdqGLvkunBQZt0" target="_blank">
+                    Resume
+                </a>
+              </p>
+              <p class="control">
+                <a class="icon" href="https://github.com/rahulpopuri">
+                    <i class="fa fa-github" aria-hidden="true"></i>
+                </a>
+              </p>
+              <p class="control">
+                <a class="icon" href="https://www.linkedin.com/in/rahulpopuri/">
+                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang='sass' scoped>
+@import 'mq'
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="home has-navbar-fixed-top">
+        <section class="hero is-light is-fullheight">
+            <div class="hero-head">
+                <div class="container is-fluid has-text-centered">
+                    <img src="../assets/rahul.jpg" width="200" height="400" alt="Rahul Popuri">
+                    <h1 class="title">
+                        Rahul Popuri
+                    </h1>
+                    <h2 class="subtitle">
+                        Java Developer
+                    </h2>
+                </div>
+            </div>
+            <div class="hero-body">
+                <div class="container is-fluid has-text-centered">
+                    <h2 class="title">
+                        Skills
+                    </h2>
+                    <span class="tag is-info">Java</span>
+                    <span class="tag is-info">Spring Boot</span>
+                    <span class="tag is-info">SQL Server</span>
+                    <span class="tag is-info">MySQL</span>
+                    <span class="tag is-info">Google Cloud</span>
+                    <span class="tag is-warning">Android</span>
+                    <span class="tag is-warning">Tableau</span>
+                    <span class="tag is-warning">Python</span>
+                    <span class="tag is-warning">MongoDB</span>
+                    <span class="tag is-warning">Redis</span>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'about',
+  data: function () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='sass' scoped>
+@import '../mq'
+
+</style>
